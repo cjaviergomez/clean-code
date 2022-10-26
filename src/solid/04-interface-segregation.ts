01db236615fbc2d228e1b489dfe837e87b757031
Carlos Gomez <javiergomez0214@gmail.com>
2022-10-26 02:54:25 -0500

    feat: add ISP

---
+++ src/solid/04-interface-segregation.ts
@@ -0,0 +1,27 @@
+// interface Bird {
+// 	eat(): void;
+// 	fly(): void;
+// 	run(): void;
+// 	swim(): void;
+// }
+
+// class Tucan implements Bird {
+// 	public fly() {}
+// 	public eat() {}
+// 	public run() {}
+// 	public swim() {} // Error: Tucan can't swim
+// }
+
+// class Ostrich implements Bird {
+// 	public eat() {}
+// 	public run() {}
+// 	public fly() {} // Error: Ostrich can't fly
+// 	public swim() {}
+// }
+
+// class Penguin implements Bird {
+// 	public eat() {}
+// 	public swim() {}
+// 	public fly() {} // Error: Penguin can't fly
+// 	public run() {}
+// }
