5ed38b6b5f658e925970c339a8fe838602214c16
Carlos Gomez <javiergomez0214@gmail.com>
2023-01-21 21:50:41 -0500

    feat: implements inversion dependencies

---
+++ src/solid/05-dependency-a.ts
@@ -0,0 +1,14 @@
+import { PostService } from './05-dependency-b';
+import { WebAPIPostService } from './05-dependency-c';
+
+// Main
+(async () => {
+	// const provider = new LocalDataBaseService();
+	// const provider = new JsonDataBaseService();
+	const provider = new WebAPIPostService();
+	const postService = new PostService(provider);
+
+	const posts = await postService.getPosts();
+
+	console.log({ posts });
+})();
